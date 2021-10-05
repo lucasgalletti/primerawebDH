4276110996684da6d5f17cf09aceb713236d99f3
Galletti <lGalletti@asofarma.com>
2021-10-04 23:18:39 -0300

    login

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const path = require('path');
 const pathPublic = path.join(__dirname, './public');
 app.use(express.static(pathPublic));
 
-app.listen(3050, console.log('Servidor OK'));
+app.listen(3050, console.log('Servidor OK - mercadoliebre - puerto 3050'));
 app.get('/', (req,res) => {res.sendFile(path.join(__dirname, './views/home.html'))});
+app.get('/home', (req,res) => {res.sendFile(path.join(__dirname, './views/home.html'))});
+app.get('/register', (req,res) => {res.sendFile(path.join(__dirname, './views/register.html'))});
+app.get('/login', (req,res) => {res.sendFile(path.join(__dirname, './views/login.html'))});
 // app.get('/', (req,res) => {res.sendFile(__dirname + './views/home.html')});
 
 
